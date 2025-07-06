11c8d224e6def74baa1579912646099bb2101722
jagdish bhai <jagdissghimire@gmail.com>
2025-07-06 14:18:33 +0545

    chore: add TaskDao for task database operations

---
+++ lib/features/task/data/dao/task_dao.dart
@@ -0,0 +1,20 @@
+import 'package:floor/floor.dart';
+import 'package:todo_clean_arch/features/task/data/model/task_model.dart';
+
+@dao
+abstract class TaskDao {
+  @Insert(onConflict: OnConflictStrategy.abort)
+  Future<void> addTask(TaskModel task);
+
+  @update
+  Future<void> updateTask(TaskModel task);
+
+  @delete
+  Future<void> deleteTask(TaskModel task);
+
+  @Query('SELECT * FROM TaskModel')
+  Future<List<TaskModel>> getAllTasks();
+
+  @Query('SELECT * FROM TaskModel WHERE id = :id')
+  Future<TaskModel?> getTaskById(String id);
+}
