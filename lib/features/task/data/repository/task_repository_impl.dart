d00cbf94e888c5a428b676390e0e4777b7d517e8
jagdish bhai <jagdissghimire@gmail.com>
2025-07-06 14:17:49 +0545

    chore: refactor task repository interface and implementation

---
+++ lib/features/task/data/repository/task_repository_impl.dart
@@ -0,0 +1,69 @@
+import '../../../../core/domain/failures.dart';
+import '../../../../core/domain/result.dart';
+import '../../domain/entity/task.dart';
+import '../../domain/repository/task_repository.dart';
+import '../datasources/task_data_source.dart';
+import '../model/task_model.dart';
+
+class TaskRepositoryImpl implements ITaskRepository {
+  final ITaskDataSource taskDataSource;
+
+  const TaskRepositoryImpl(this.taskDataSource);
+
+  @override
+  Future<Result<bool, Failure>> deleteTask(Task task) async {
+    try {
+      final result =
+          await taskDataSource.deleteTask(TaskModel.fromEntity(task));
+      return Success(result);
+    } catch (e) {
+      return Error(CacheFailure("Failed to delete task: $e"));
+    }
+  }
+
+  @override
+  Future<Result<List<Task>, Failure>> getAllTasks() async {
+    try {
+      final taskModels = await taskDataSource.getAllTasks();
+      final tasks = taskModels.map((model) => model.toEntity()).toList();
+      return Success(tasks);
+    } catch (e) {
+      return Error(CacheFailure("Failed to fetch tasks: $e"));
+    }
+  }
+
+  @override
+  Future<Result<Task?, Failure>> getTaskById(String id) async {
+    try {
+      final taskModel = await taskDataSource.getTaskById(id);
+      if (taskModel != null) {
+        return Success(taskModel.toEntity());
+      } else {
+        return Success(null);
+      }
+    } catch (e) {
+      return Error(CacheFailure("Failed to fetch task by id: $e"));
+    }
+  }
+
+  @override
+  Future<Result<bool, Failure>> updateTask(Task task) async {
+    try {
+      final result =
+          await taskDataSource.updateTask(TaskModel.fromEntity(task));
+      return Success(result);
+    } catch (e) {
+      return Error(CacheFailure("Failed to update task: $e"));
+    }
+  }
+
+  @override
+  Future<Result<bool, Failure>> createTask(Task task) async {
+    try {
+      final result = await taskDataSource.addTask(TaskModel.fromEntity(task));
+      return Success(result);
+    } catch (e) {
+      return Error(CacheFailure("Failed to create task: $e"));
+    }
+  }
+}
--- lib/features/task/domain/repository/task_repository.dart
+++ lib/features/task/domain/repository/task_repository.dart
@@ -1,16 +1,16 @@
 import 'package:todo_clean_arch/core/domain/result.dart';
 
 import '../../../../core/domain/failures.dart';
-import '../entities/task.dart';
+import '../entity/task.dart';
 
 abstract class ITaskRepository {
   Future<Result<List<Task>, Failure>> getAllTasks();
 
   Future<Result<Task?, Failure>> getTaskById(String id);
 
-  Future<Result<bool, Failure>> addTask(Task task);
+  Future<Result<bool, Failure>> createTask(Task task);
 
   Future<Result<bool, Failure>> updateTask(Task task);
 
-  Future<Result<bool, Failure>> deleteTask(String id);
+  Future<Result<bool, Failure>> deleteTask(Task id);
 }
