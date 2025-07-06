a1d3a48b60b2d1ed456958a84eb837676143137a
jagdish bhai <jagdissghimire@gmail.com>
2025-07-06 20:16:10 +0545

    chore: refactor ListTasksPage to use Bloc for state management

--- lib/features/task/presentation/pages/list_tasks_page.dart
+++ lib/features/task/presentation/pages/list_tasks_page.dart
@@ -1,142 +1,129 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 import '../../domain/entity/task.dart';
+import '../bloc/task_bloc/task_bloc.dart';
+import '../bloc/task_bloc/task_event.dart';
+import '../bloc/task_bloc/task_state.dart';
+import '../widget/create_task_widget.dart';
 import '../widget/task_widget.dart';
 
-class ListTasksPage extends StatefulWidget {
+class ListTasksPage extends StatelessWidget {
   const ListTasksPage({super.key});
 
   @override
-  State<ListTasksPage> createState() => _ListTasksPageState();
-}
-
-class _ListTasksPageState extends State<ListTasksPage> {
   @override
   Widget build(BuildContext context) {
     final appSize = MediaQuery.of(context).size;
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("My Tasks"),
-          centerTitle: false,
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.filter_list),
-              onPressed: () {
-                // Handle search action
-                print("Search tasks");
-              },
-            ),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            // Handle add task action
-            print("Add new task");
-          },
-          child: const Icon(Icons.add),
-        ),
-        body: SafeArea(
-          child: Builder(builder: (ctx) {
-            if (appSize.width < 400) {
-              return ListView(
-                children: dummyTasks
-                    .map((dummyTasks) => TaskWidget(
-                          task: dummyTasks,
-                          onDelete: () {
-                            // Handle delete action
-                            print("Delete task: ${dummyTasks.title}");
-                          },
-                          onUpdate: () {
-                            // Handle update action
-                            print("Update task: ${dummyTasks.title}");
-                          },
-                        ))
-                    .toList(),
-              );
-            } else if (appSize.width < 900) {
-              return MasonryGridView.builder(
-                gridDelegate: SliverSimpleGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2),
-                itemCount: dummyTasks.length,
-                itemBuilder: (context, index) {
-                  final task = dummyTasks[index];
-                  return TaskWidget(
-                    task: task,
-                    onDelete: () {
-                      // Handle delete action
-                      print("Delete task: ${task.title}");
-                    },
-                    onUpdate: () {
-                      // Handle update action
-                      print("Update task: ${task.title}");
-                    },
-                  );
+
+    return BlocConsumer<TaskBloc, TaskState>(
+      listener: (ctx, state) {
+        final List<Task> tasksLists = [];
+        if (state is TaskLoadedState) {
+          tasksLists.addAll(state.tasks);
+        } else if (state is TaskOperationErrorState) {
+          tasksLists.addAll(state.tasks);
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text("Error: ${state.message}")),
+          );
+        }
+      },
+      builder: (context, state) {
+        if (state is TaskLoadingState) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        }
+
+        if (state is TaskErrorState) {
+          return Scaffold(
+            appBar: AppBar(title: const Text("My Tasks")),
+            body: Center(child: Text("Error: ${state.message}")),
+          );
+        }
+
+        // Shared state for Loaded & OperationError
+        List<Task> tasks = [];
+
+        if (state is TaskLoadedState) {
+          tasks = state.tasks;
+        } else if (state is TaskOperationErrorState) {
+          tasks = List.from(state.tasks)
+            ..sort((a, b) => b.createdAt.compareTo(a.createdAt));
+        }
+
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text("My Tasks"),
+            centerTitle: false,
+            actions: [
+              PopupMenuButton<String>(
+                icon: const Icon(Icons.tune),
+                onSelected: (value) {
+                  if (value == "completed") {
+                    context.read<TaskBloc>().add(
+                          FilterTasksEvent(true),
+                        );
+                  } else if (value == "incomplete") {
+                    context.read<TaskBloc>().add(
+                          FilterTasksEvent(false),
+                        );
+                  } else if (value == "reset") {
+                    context.read<TaskBloc>().add(
+                          FilterTasksEvent(null),
+                        );
+                  }
                 },
-              );
-            } else {
-              return MasonryGridView.builder(
-                gridDelegate:
-                    const SliverSimpleGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                ),
-                itemCount: dummyTasks.length,
-                itemBuilder: (context, index) {
-                  final task = dummyTasks[index];
-                  return TaskWidget(
-                    task: task,
-                    onDelete: () {
-                      // Handle delete action
-                      print("Delete task: ${task.title}");
-                    },
-                    onUpdate: () {
-                      // Handle update action
-                      print("Update task: ${task.title}");
-                    },
-                  );
-                },
-              );
-            }
-          }),
-        ));
+                itemBuilder: (context) => [
+                  const PopupMenuItem(
+                    value: "completed",
+                    child: Text('Completed'),
+                  ),
+                  const PopupMenuItem(
+                    value: "incomplete",
+                    child: Text('Incomplete'),
+                  ),
+                  const PopupMenuItem(
+                    value: "reset",
+                    child: Text('All'),
+                  ),
+                ],
+              ),
+
+              //sort by date
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            heroTag: "uniqueHero",
+            onPressed: () async {
+              await showTaskFormDialog(context);
+            },
+            child: const Icon(Icons.add),
+          ),
+          body: SafeArea(
+            child: tasks.isEmpty
+                ? const Center(child: Text("No tasks available"))
+                : (appSize.width < 400
+                    ? ListView(
+                        children: tasks
+                            .map((task) => TaskWidget(task: task))
+                            .toList(),
+                      )
+                    : MasonryGridView.builder(
+                        gridDelegate:
+                            SliverSimpleGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: appSize.width < 900 ? 2 : 3,
+                        ),
+                        itemCount: tasks.length,
+                        itemBuilder: (context, index) {
+                          final task = tasks[index];
+                          return TaskWidget(task: task);
+                        },
+                      )),
+          ),
+        );
+      },
+    );
   }
 }
-
-final List<Task> dummyTasks = [
-  Task(
-    id: '1',
-    title: 'Buy groceries',
-    description: 'Milk, eggs, bread, fruits',
-    createdAt: DateTime.now().subtract(const Duration(days: 1)),
-    priority: TaskPriority.high,
-  ),
-  Task(
-    id: '2',
-    title: 'Finish Flutter project',
-    description: 'Implement UI for the task list screen',
-    createdAt: DateTime.now().subtract(const Duration(days: 2)),
-    priority: TaskPriority.medium,
-  ),
-  Task(
-    id: '3',
-    title: 'Call electrician',
-    description: 'Fix the kitchen light issue',
-    createdAt: DateTime.now().subtract(const Duration(hours: 6)),
-    isCompleted: true,
-    priority: TaskPriority.low,
-  ),
-  Task(
-    id: '4',
-    title: 'Read a book',
-    description: 'Start reading "Atomic Habits"',
-    createdAt: DateTime.now().subtract(const Duration(days: 4)),
-    priority: TaskPriority.low,
-  ),
-  Task(
-    id: '5',
-    title: 'Workout',
-    description: 'Upper body session at the gym',
-    createdAt: DateTime.now(),
-    priority: TaskPriority.high,
-  ),
-];
